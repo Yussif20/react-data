d685f9f1e826f0b588f1ea045f314b969d5a6a56
Yousef <yousefayman2000500@gmail.com>
2023-07-01 21:49:32 +0300

    adding the navbar for mobile screen without the functionality

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,13 +2,26 @@ const Navbar = () => {
   return (
     <div className="text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4">
       <h1 className="w-full text-3xl font-bold text-[#00df9a]">React.</h1>
-      <ul className="flex">
+      <ul className="flex hidden">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
+      <div></div>
+      <div className="fixed left-0 top-0 bg-[#000300] w-[60%] h-full border-r border-gray-900">
+        <h1 className="w-full text-3xl font-bold text-[#00df9a] mc-4">
+          React.
+        </h1>
+        <ul className="uppercase p-4 ">
+          <li className="p-4 border-b border-gray-600">Home</li>
+          <li className="p-4 border-b border-gray-600">Company</li>
+          <li className="p-4 border-b border-gray-600">Resources</li>
+          <li className="p-4 border-b border-gray-600">About</li>
+          <li className="p-4 border-b border-gray-600">Contact</li>
+        </ul>
+      </div>
     </div>
   );
 };
