929073006c59e3229709814347d40451cb86dd92
Yousef <yousefayman2000500@gmail.com>
2023-07-01 21:25:47 +0300

    adding the nav for computer screens

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,5 +1,16 @@
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="text-white flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4">
+      <h1 className="w-full text-3xl font-bold text-[#00df9a]">React.</h1>
+      <ul className="flex">
+        <li className="p-4">Home</li>
+        <li className="p-4">Company</li>
+        <li className="p-4">Resources</li>
+        <li className="p-4">About</li>
+        <li className="p-4">Contact</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
